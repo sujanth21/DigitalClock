9331a9dd2df85fb0011d83fd3f0e4f68c38eafb5
Sujanth <sujanth1987@gmail.com>
2017-08-20 10:03:48 +1000

    Digital Clock UI completed

--- DigitalClock/ViewController.swift
+++ DigitalClock/ViewController.swift
@@ -10,14 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var clockLabel: UILabel!
+    @IBOutlet weak var settingsBtn: UIButton!
+    @IBOutlet weak var clockColorSegCtrl: UISegmentedControl!
+    @IBOutlet weak var backgroundColorSegCtrl: UISegmentedControl!
+    @IBOutlet weak var settingsView: UIView!
+    
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.updateTimer()
+        settingsView.isHidden = true
+        settingsBtn.alpha = 0.25
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func settingsBtnPressed(_ sender: Any) {
+        
+        if (settingsView.isHidden == true) {
+            settingsView.isHidden = false
+            settingsBtn.setTitle("Hide Settings", for: .normal)
+            settingsBtn.alpha = 1.0
+        } else {
+            settingsView.isHidden = true
+            settingsBtn.setTitle("Show Settings", for: .normal)
+            settingsBtn.alpha = 0.25
+        }
+    
+    }
+    
+    
+    
+    @objc func updateTimer() {
+        
+        let timeFormatter = DateFormatter()
+        timeFormatter.timeStyle = .medium
+        
+        clockLabel.text = timeFormatter.string(from: Date())
     }
 
 
